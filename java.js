33cec1ecaeeaf090f2c59b31686004c83f7e2fae
David Rocendo <rocendod@msu.edu>
2024-03-17 22:13:34 -0400

    fixed the issue with importing supabase within java.js

--- java.js
+++ java.js
@@ -1,14 +1,18 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabaseUrl = 'https://tekryqsppwpsuvvsnetl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRla3J5cXNwcHdwc3V2dnNuZXRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MDA4NzYsImV4cCI6MjAyNjE3Njg3Nn0.4L5FJ-gAa0iFgvbfLkQMW3jMVRGzI2UTi9wnsc7z4r8'
 const supabase = createClient(supabaseUrl, supabaseKey)
+console.log('Supabase Instance: ', supabase)
 
+async function getBooks() {
+        let { data: books, error } = await supabase
+        .from('books')
+        .select('*')
 
-let { data: books, error } = await supabase
-  .from('books')
-  .select('*')
-
-for (let book of books) {
-  let bookList = document.getElementById('books');
-  bookList.innerHTML += `<li>${book.title} - ${book.Author}</li>`;
+        for (let book of books) {
+        let bookList = document.getElementById('books');
+        bookList.innerHTML += `<li>${book.title} - ${book.Author}</li>`;
+    }
 }
+
+getBooks();
